e801cef8e305dd9a98dfeb4eebc72e6d05a62158
tkm2217 <muhammadtalha@my.unt.edu>
2021-11-06 14:46:54 -0500

    !!

--- src/main/java/hashing/MyHashTable.java
+++ src/main/java/hashing/MyHashTable.java
@@ -13,24 +13,11 @@ public class MyHashTable<K,V>
         extends Dictionary<K,V>
         implements Map<K,V>, Cloneable, java.io.Serializable {
 
-
     private transient Entry<?,?>[] table;
-
-
     private transient int count;
-
-
     private int threshold;
-
-
     private float loadFactor;
-
-
     private transient int modCount = 0;
-
-
-    private static final long serialVersionUID = 1421746759512286392L;
-
 
     public MyHashTable(int initialCapacity, float loadFactor) {
         if (initialCapacity < 0)
@@ -46,39 +33,30 @@ public class MyHashTable<K,V>
         threshold = (int)Math.min(initialCapacity * loadFactor, MAX_ARRAY_SIZE + 1);
     }
 
-
-
     public MyHashTable() {
         this(11, 0.75f);
     }
-
-
 
     public synchronized int size() {
         return count;
     }
 
-
     public synchronized boolean isEmpty() {
         return count == 0;
     }
-
 
     public synchronized Enumeration<K> keys() {
         return this.<K>getEnumeration(KEYS);
     }
 
-
     public synchronized Enumeration<V> elements() {
         return this.<V>getEnumeration(VALUES);
     }
-
 
     public synchronized boolean contains(Object value) {
         if (value == null) {
             throw new NullPointerException();
         }
-
         Entry<?,?> tab[] = table;
         for (int i = tab.length ; i-- > 0 ;) {
             for (Entry<?,?> e = tab[i] ; e != null ; e = e.next) {
@@ -90,11 +68,9 @@ public class MyHashTable<K,V>
         return false;
     }
 
-
     public boolean containsValue(Object value) {
         return contains(value);
     }
-
 
     public synchronized boolean containsKey(Object key) {
         Entry<?,?> tab[] = table;
@@ -107,7 +83,6 @@ public class MyHashTable<K,V>
         }
         return false;
     }
-
 
     @SuppressWarnings("unchecked")
     public synchronized V get(Object key) {
@@ -122,9 +97,7 @@ public class MyHashTable<K,V>
         return null;
     }
 
-
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-
 
     @SuppressWarnings("unchecked")
     protected void rehash() {
@@ -232,9 +205,6 @@ public class MyHashTable<K,V>
             put(e.getKey(), e.getValue());
     }
 
-    /**
-     * Clears this hashtable so that it contains no keys.
-     */
     public synchronized void clear() {
         Entry<?,?> tab[] = table;
         modCount++;
@@ -284,32 +254,11 @@ public class MyHashTable<K,V>
         }
     }
 
-    // Views
 
-    /**
-     * Each of these fields are initialized to contain an instance of the
-     * appropriate view the first time this view is requested.  The views are
-     * stateless, so there's no reason to create more than one of each.
-     */
     private transient volatile Set<K> keySet = null;
     private transient volatile Set<Map.Entry<K,V>> entrySet = null;
     private transient volatile Collection<V> values = null;
 
-    /**
-     * Returns a {@link Set} view of the keys contained in this map.
-     * The set is backed by the map, so changes to the map are
-     * reflected in the set, and vice-versa.  If the map is modified
-     * while an iteration over the set is in progress (except through
-     * the iterator's own <tt>remove</tt> operation), the results of
-     * the iteration are undefined.  The set supports element removal,
-     * which removes the corresponding mapping from the map, via the
-     * <tt>Iterator.remove</tt>, <tt>Set.remove</tt>,
-     * <tt>removeAll</tt>, <tt>retainAll</tt>, and <tt>clear</tt>
-     * operations.  It does not support the <tt>add</tt> or <tt>addAll</tt>
-     * operations.
-     *
-     * @since 1.2
-     */
     public Set<K> keySet() {
         if (keySet == null)
             keySet = Collections.synchronizedSet(new KeySet());
@@ -423,17 +372,6 @@ public class MyHashTable<K,V>
         }
     }
 
-    // Comparison and hashing
-
-    /**
-     * Compares the specified Object with this Map for equality,
-     * as per the definition in the Map interface.
-     *
-     * @param  o object to be compared for equality with this hashtable
-     * @return true if the specified Object is equal to this Map
-     * @see Map#equals(Object)
-     * @since 1.2
-     */
     public synchronized boolean equals(Object o) {
         if (o == this)
             return true;
@@ -467,24 +405,8 @@ public class MyHashTable<K,V>
         return true;
     }
 
-    /**
-     * Returns the hash code value for this Map as per the definition in the
-     * Map interface.
-     *
-     * @see Map#hashCode()
-     * @since 1.2
-     */
     public synchronized int hashCode() {
-        /*
-         * This code detects the recursion caused by computing the hash code
-         * of a self-referential hash table and prevents the stack overflow
-         * that would otherwise result.  This allows certain 1.1-era
-         * applets with self-referential hash tables to work.  This code
-         * abuses the loadFactor field to do double-duty as a hashCode
-         * in progress flag, so as not to worsen the space performance.
-         * A negative load factor indicates that hash code computation is
-         * in progress.
-         */
+
         int h = 0;
         if (count == 0 || loadFactor < 0)
             return h;  // Returns zero
@@ -497,36 +419,8 @@ public class MyHashTable<K,V>
                 entry = entry.next;
             }
         }
-
         loadFactor = -loadFactor;  // Mark hashCode computation complete
-
         return h;
-    }
-
-    @Override
-    public synchronized V getOrDefault(Object key, V defaultValue) {
-        V result = get(key);
-        return (null == result) ? defaultValue : result;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public synchronized void forEach(BiConsumer<? super K, ? super V> action) {
-        Objects.requireNonNull(action);     // explicit check required in case
-        // table is empty.
-        final int expectedModCount = modCount;
-
-        Entry<?, ?>[] tab = table;
-        for (Entry<?, ?> entry : tab) {
-            while (entry != null) {
-                action.accept((K)entry.key, (V)entry.value);
-                entry = entry.next;
-
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-            }
-        }
     }
 
     @Override
@@ -554,9 +448,6 @@ public class MyHashTable<K,V>
         return false;
     }
 
-    /**
-     * Hashtable bucket collision list entry
-     */
     private static class Entry<K,V> implements Map.Entry<K,V> {
         final int hash;
         final K key;
@@ -575,8 +466,6 @@ public class MyHashTable<K,V>
             return new Entry<>(hash, key, value,
                     (next==null ? null : (Entry<K,V>) next.clone()));
         }
-
-        // Map.Entry Ops
 
         public K getKey() {
             return key;
@@ -618,13 +507,7 @@ public class MyHashTable<K,V>
     private static final int VALUES = 1;
     private static final int ENTRIES = 2;
 
-    /**
-     * A hashtable enumerator class.  This class implements both the
-     * Enumeration and Iterator interfaces, but individual instances
-     * can be created with the Iterator methods disabled.  This is necessary
-     * to avoid unintentionally increasing the capabilities granted a user
-     * by passing an Enumeration.
-     */
+
     private class Enumerator<T> implements Enumeration<T>, Iterator<T> {
         Entry<?,?>[] table = MyHashTable.this.table;
         int index = table.length;
@@ -632,17 +515,8 @@ public class MyHashTable<K,V>
         Entry<?,?> lastReturned = null;
         int type;
 
-        /**
-         * Indicates whether this Enumerator is serving as an Iterator
-         * or an Enumeration.  (true -> Iterator).
-         */
         boolean iterator;
 
-        /**
-         * The modCount value that the iterator believes that the backing
-         * Hashtable should have.  If this expectation is violated, the iterator
-         * has detected concurrent modification.
-         */
         protected int expectedModCount = modCount;
 
         Enumerator(int type, boolean iterator) {
